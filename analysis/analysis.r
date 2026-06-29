b12b997e3052edb45445b496853c94929fdd1c75
Sebastian Kappes <sebastian.kappes@uni-konstanz.de>
2026-06-29 12:40:06 +0200

    Initial commit

---
+++ analysis/analysis.r
@@ -0,0 +1,7 @@
+library(dplyr)
+
+renv::snapshot()
+  #scans the defined folder (or working directory) for usage of any packages anywhere
+  #to then save all the necessary information to reproduce into the renv.lock file
+
+
